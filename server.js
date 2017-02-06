8895ed9b698e3868ba5b1f6798dac9b99c307a15
Nasser M. Tabook <Tabook22@Nassers-MacBook-Pro.local>
2017-02-06 11:29:28 +0400

    First commit

---
+++ server.js
@@ -0,0 +1,20 @@
+var express =require('express');
+var path=require('path');
+var bodyParser=require('body-parser');
+
+var port=process.env.PORT || 8000;
+
+var app=express();
+
+//User Middelwears
+app.use(express.static(path.join(__dirname,"public")));
+
+app.get('/', function(req,res){
+    res.send("آستغفر الله و اتوب اليه");
+});
+
+//Run the server
+
+app.listen(port, function(){
+    console.log("Server is Running at Port: "+8000);
+});
